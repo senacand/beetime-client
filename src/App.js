e29e8ad2b203d7a7f419da50e04e2cf6efeb6163
Sena Candra MPC <senacandrampc@gmail.com>
2017-12-28 23:27:01 +0800

    Test Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            <Route path="/" exact component={home} />
+            <Route path="/s/:id" component={test}/>
+          </p>
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+const test = ({ match }) => (
+  <div>
+    {match.params.id}
+  </div>
+);
+
+const home = () => (
+  <div>
+    <code>/s/:id</code>
+  </div>
+)
 
 export default App;
